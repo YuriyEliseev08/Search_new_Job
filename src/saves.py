5d8f8b51ec2c1ab09a91b7b94295bdf157437a63
Yuriy Eliseev <136329249+YuriyEliseev08@users.noreply.github.com>
2023-11-19 11:07:10 +0700

    прописаны докстрингки к функциям

--- src/saves.py
+++ src/saves.py
@@ -7,16 +7,19 @@ class JsonSave:
 
     @staticmethod
     def delete_file():
+        """Удаляет файл"""
         if os.path.exists('vacancies.json'):
             os.remove('vacancies.json')
 
     @classmethod
     def get_vacancies(self):
+        """Записывает данные в файл"""
         with open('vacancies.json', 'r', encoding='utf-8') as json_file:
             data = json.load(json_file)
             return data
 
 
 def add_vacancy():
+    """Открывает созданный файл"""
     with open('vacancies.json', 'a', encoding='utf-8') as json_file:
         json.dump(JsonSave.all_vacancies, json_file, indent=4, ensure_ascii=False)
--- src/vacancies.py
+++ src/vacancies.py
@@ -4,55 +4,68 @@ from src.saves import JsonSave
 class Vacancy:
     all = []
 
-    def __init__(self, name, url, salary, exp):
-        self.name = name
-        self.url = url
-        self.salary = salary
-        self.exp = exp
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', '{self.url}', '{self.salary}', '{self.exp}')"
+"""Инициализируем параметры по вакансиям"""
 
-    def __str__(self):
-        return (f"{self.__class__.__name__}('Професия: {self.name}', 'Страничка оъявления: {self.url}',"
-                f" 'Зарплата: {self.salary}', 'Опыт: {self.exp}')")
 
-    def __ge__(self, other):
-        # Сравнение по зарплате для текущей вакансии и другой вакансии (other)
-        return self.salary >= other.salary
+def __init__(self, name, url, salary, exp):
+    self.name = name
+    self.url = url
+    self.salary = salary
+    self.exp = exp
 
-    def __lt__(self, other):
-        # Сравнение по зарплате для текущей вакансии и другой вакансии (other)
-        return self.salary < other.salary
 
-    def __eq__(self, other):
-        # Сравнение по зарплате для текущей вакансии и другой вакансии (other)
-        return self.salary == other.salary
+def __repr__(self):
+    return f"{self.__class__.__name__}('{self.name}', '{self.url}', '{self.salary}', '{self.exp}')"
 
-    @staticmethod
-    def compare_vacancies_by_salary():
-        data = JsonSave.get_vacancies()  # Вызываем функцию, чтобы получить данные
-        if data:
-            vacancies = [Vacancy(v['name'], v['url'], v['salary'], v['experience']) for v in data['vacancies']]
-            sorted_vacancies = sorted(vacancies, key=lambda vacancy: vacancy.salary, reverse=True)
-            Vacancy.all = sorted_vacancies
-            return sorted_vacancies
-        else:
-            print("Нет данных о вакансиях.")
-            return []
 
-    def sort_vacancy(self):
-        pass
+def __str__(self):
+    """Возвращаем метод str для отображения"""
+    return (f"{self.__class__.__name__}('Професия: {self.name}', 'Страничка оъявления: {self.url}',"
+            f" 'Зарплата: {self.salary}', 'Опыт: {self.exp}')")
 
-    @staticmethod
-    def print_vacancies(limit=20):
-        """Печатает информацию о вакансиях в консоль, ограничивая количество вакансий до заданного лимита."""
-        for_print = Vacancy.all
-        count = 1
-        for vacancy in for_print:
-            if count > limit:
-                break  # Завершаем цикл, если достигнуто заданное ограничение
-            print(
-                f'Вакансия №{count}:\nНазвание:{vacancy.name}\n'
-                f'Зарплата:{vacancy.salary} рублей\nОпыт работы: {vacancy.exp}\nСсылка на сайт: {vacancy.url}\n')
-            count += 1
+
+def __ge__(self, other):
+    """Сравнение по зарплате для текущей вакансии и другой вакансии (other)"""
+    return self.salary >= other.salary
+
+
+def __lt__(self, other):
+    """Сравнение по зарплате для текущей вакансии и другой вакансии (other)"""
+    return self.salary < other.salary
+
+
+def __eq__(self, other):
+    """Сравнение по зарплате для текущей вакансии и другой вакансии (other)"""
+    return self.salary == other.salary
+
+
+@staticmethod
+def compare_vacancies_by_salary():
+    data = JsonSave.get_vacancies()  # Вызываем функцию, чтобы получить данные
+    if data:
+        vacancies = [Vacancy(v['name'], v['url'], v['salary'], v['experience']) for v in data['vacancies']]
+        sorted_vacancies = sorted(vacancies, key=lambda vacancy: vacancy.salary, reverse=True)
+        Vacancy.all = sorted_vacancies
+        return sorted_vacancies
+    else:
+        print("Нет данных о вакансиях.")
+        return []
+
+
+def sort_vacancy(self):
+    pass
+
+
+@staticmethod
+def print_vacancies(limit=20):
+    """Печатает информацию о вакансиях в консоль, ограничивая количество вакансий до заданного лимита."""
+    for_print = Vacancy.all
+    count = 1
+    for vacancy in for_print:
+        if count > limit:
+            break  # Завершаем цикл, если достигнуто заданное ограничение
+        print(
+            f'Вакансия №{count}:\nНазвание:{vacancy.name}\n'
+            f'Зарплата:{vacancy.salary} рублей\nОпыт работы: {vacancy.exp}\nСсылка на сайт: {vacancy.url}\n')
+        count += 1
