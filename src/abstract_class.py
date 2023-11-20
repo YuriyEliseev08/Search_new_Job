99a64288bcda2017f7a15ebc45b6ac33b134a1cf
Yuriy Eliseev <136329249+YuriyEliseev08@users.noreply.github.com>
2023-11-20 17:36:16 +0700

    abstract_class.py добавлен проект

---
+++ src/abstract_class.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class API(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+    @abstractmethod
+    def load_jobs(self, keywords, location):
+        pass
+
+    @abstractmethod
+    def format_vacancies(self, all_vacancies):
+        pass
